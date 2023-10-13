4e6407160d79ea167cd9bc4f1068b610aa868082
hunnit-zae <100geonjae@gmail.com>
2023-10-12 23:37:33 -0400

    initial commit

---
+++ app/src/main/java/com/example/android_project/Tag.java
@@ -0,0 +1,30 @@
+package com.example.android_project;
+
+import java.io.Serializable;
+
+public class Tag implements Serializable {
+
+    private String name;
+    private String value;
+
+    public Tag(String name, String value) {
+        this.name=name;
+        this.value=value;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public String getValue(){
+        return value;
+    }
+
+    public String returnTag() {
+        return name+"="+value;
+    }
+
+    public String toString() {
+        return name+"="+value;
+    }
+
+}
